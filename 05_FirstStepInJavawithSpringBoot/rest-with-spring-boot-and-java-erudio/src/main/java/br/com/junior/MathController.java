b9a779106efd11a34d1b9bcd06c8a6ce0121f899
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2023-04-02 14:45:28 -0300

    UP
    Aula 54 finalizada

--- 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/junior/MathController.java
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/junior/MathController.java
@@ -1,21 +1,19 @@
 package br.com.junior;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class MathController {
 	
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	//private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
 		
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return 1D;
 	}
 	
 }

e5d7260a2a272a8f7cd39bd565e028e10d8f43af
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2023-04-03 07:21:20 -0300

    up
    up

--- 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/junior/MathController.java
+++ 05_FirstStepInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/junior/MathController.java
@@ -5,15 +5,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.junior.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 	
 	//private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
-	public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+	public Double sum(@PathVariable(value = "numberOne") String numberOne, 
+					  @PathVariable(value = "numberTwo") String numberTwo
+		) throws Exception {
 		
-		return 1D;
+		// validando se a string é numerica ou não
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	
+	// conversão para Double
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) {
+			return 0D;
+		}
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		if (isNumeric(number)) {
+			return Double.parseDouble(number);
+		}
+		return 0D;
+	}
+
+	// verificação se é numeric
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) {
+			return false;
+		}
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 	
 }
